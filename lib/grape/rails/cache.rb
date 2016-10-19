35a8f55c066e3f493152ccd0a7dfb36bda7878ac
dwgeneral <guowind@live.com>
2016-10-19 22:16:07 +0800

    delete puts

--- lib/grape/rails/cache.rb
+++ lib/grape/rails/cache.rb
@@ -59,7 +59,6 @@ module Grape
             # Try to fetch from server side cache
             cache_store_expire_time = (opts[:cache_store_expires_in] || opts[:expires_in] || default_expire_time).to_i
             if cache_store_expire_time <= 0
-              puts "------ cache store expire time <= 0 ------"
               block.call
             else
               puts "------------- else ---------------"
