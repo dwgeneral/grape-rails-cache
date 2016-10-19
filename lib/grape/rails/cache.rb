9c6d474c539283a3b88056b0234a1eb7e748e398
dwgeneral <guowind@live.com>
2016-10-19 21:58:46 +0800

    add puts again

--- lib/grape/rails/cache.rb
+++ lib/grape/rails/cache.rb
@@ -59,8 +59,10 @@ module Grape
             # Try to fetch from server side cache
             cache_store_expire_time = (opts[:cache_store_expires_in] || opts[:expires_in] || default_expire_time).to_i
             if cache_store_expire_time <= 0
-              block.call.to_s.to_json
+              puts "------------- cache store expire time <= 0 ---------------"
+              block.call.to_json
             else
+              puts "------------- else ---------------"
              # ::Rails.cache.fetch(cache_key, raw: true, expires_in: cache_store_expire_time) do
               ::Rails.cache.fetch(cache_key, expires_in: cache_store_expire_time) do
                 block.call.to_s.to_json
