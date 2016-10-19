f2c59bc76cd50d8337fbf9bc48fca0f838b47b0f
dwgeneral <guowind@live.com>
2016-10-19 19:36:03 +0800

    delete raw: true

--- lib/grape/rails/cache.rb
+++ lib/grape/rails/cache.rb
@@ -56,10 +56,11 @@ module Grape
             # Try to fetch from server side cache
             cache_store_expire_time = (opts[:cache_store_expires_in] || opts[:expires_in] || default_expire_time).to_i
             if cache_store_expire_time <= 0
-              block.call.to_s
+              block.call
             else
-              ::Rails.cache.fetch(cache_key, raw: true, expires_in: cache_store_expire_time) do
-                block.call.to_s
+             # ::Rails.cache.fetch(cache_key, raw: true, expires_in: cache_store_expire_time) do
+              ::Rails.cache.fetch(cache_key, expires_in: cache_store_expire_time) do
+                block.call
               end
             end
           end
